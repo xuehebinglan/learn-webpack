73a0478a3a9f08d9642ca8ce522563780577606e
xuehebinglan <785529534@qq.com>
2019-03-12 15:00:06 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './miao.jpg'
 
 function component() {
     var element = document.createElement('div');
@@ -8,6 +9,12 @@ function component() {
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
